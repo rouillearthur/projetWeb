3bb7a6a6956899ffd06a3e7a843a8db6fb4d6796
ROUILLE arthur <arouille@e205pc08.ecole.ensicaen.fr>
2018-12-14 10:35:09 +0100

    qlq modifs

---
+++ tools/tools.js
@@ -0,0 +1,12 @@
+function truncate(val)
+{
+	return (intval = val - val%1);
+}
+
+function getRandomInt(min, max)
+{
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandom(min, max)
+{ return (Math.random() * (max-min+1)) + min; }
